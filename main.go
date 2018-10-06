c3333fdded834df7bb634c6959bf7638fd42c005
Nik Bond <nikbond97@gmail.com>
2018-10-06 20:56:39 +0300

    Add argparsing

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"echoServer/echoserver"
+	"flag"
 	"fmt"
 )
 
-const (
-	IP   = "127.0.0.1"
-	PORT = "12345"
+var (
+	ip   string
+	port string
 )
 
+func init() {
+	flag.StringVar(&ip, "ip", "", "server ip address")
+	flag.StringVar(&port, "port", "", "server port")
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println("Server run on", IP+":"+PORT)
-	server := &echoserver.Server{Ip: IP, Port: PORT}
+	fmt.Println("Server run on", ip+":"+port)
+	server := &echoserver.Server{Ip: ip, Port: port}
 	server.Run()
 	return
 }
